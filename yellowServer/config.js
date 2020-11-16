e025457d6b0171c15c9d4e9766dcac54bfdb23f5
James Vinson <vinsonjames02@gmail.com>
2020-11-15 21:47:04 -0500

    fixing debugging issues. addd  require('path') for relative paths

--- yellowServer/config.js
+++ yellowServer/config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-let data_config = fs.readFileSync('c.json');
+const path = require('path');
+let data_config = fs.readFileSync(path.resolve(__dirname, 'c.json'));
 let config = JSON.parse(data_config);
 let hostname = config.hostname;
 let port = config.port;
